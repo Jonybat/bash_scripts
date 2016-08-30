d01362f6aeb2fcb924f166c0417f6c62d63b66fd
Jonybat <jonybat26@hotmail.com>
2016-08-30 16:47:50 +0100

    Invert global variables logic and clean up a bit

--- shlog.sh
+++ shlog.sh
@@ -11,134 +11,121 @@
 ### Can change this variable to set a default global directory. Can still be overridden by the global variables
 globalLogDir="/var/log/scripts/"
 
+
 shlog_validate_dir ()
 {
 # Check if dir doesnt exist and try to create it. Check if it exists and that it is a directory and writable
 if [[ ! -e "$1" ]]; then
 	mkdir -p "$1"
 	if [[ $? -ne 0 ]]; then
-		echo "ERROR! - LOGDIR ($1) does not exist and can not be created. Exiting..."
+		echo "ERROR! - shlogDir ($1) does not exist and can not be created. Exiting..."
 		exit 1
 	fi
 elif [[ ! -d "$1" ]]; then
-	echo "ERROR! - LOGDIR ($1) exists but is not a directory. Exiting..."
+	echo "ERROR! - shlogDir ($1) exists but is not a directory. Exiting..."
 	exit 1
 elif [[ ! -w "$1" ]]; then
-	echo "ERROR! - LOGDIR ($1) exists but is not writable. Exiting..."
+	echo "ERROR! - shlogDir ($1) exists but is not writable. Exiting..."
 	exit 1
 fi
 
 # Check if dir has a leading slash and add it if it doesnt
 if [[ ! "$1" =~ /$ ]]; then
-	LOGDIR="$1"/
+	shlogDir="$1"/
 else
-	LOGDIR="$1"
+	shlogDir="$1"
 fi
 }
 
 
 shlog_logdir ()
 {
-# Default log directory is the script dir. If set by parent script, unset LOGPATH to allow this script to build it
+# Default log directory is the script dir. If set by parent script, unset shlogPath to allow this script to build it
 if [[ -z "$LOGDIR" ]]; then
 	if [[ -z "$globalLogDir" ]]; then
 		relativeLogDir=$(dirname "$0")
-		LOGDIR=$(cd "$relativeLogDir" && pwd)
+		shlogDir=$(cd "$relativeLogDir" && pwd)
 	else
-		LOGDIR="$globalLogDir"
+		shlogDir="$globalLogDir"
 	fi
 else
-	unset LOGPATH
+	shlogDir="$LOGDIR"
+#	unset shlogPath
 fi
 }
+
 
 shlog_logfile ()
 {
 # If not set by the parent script, get its name, filter the path and replace extension with .log, if it has one
 if [[ -z "$LOGFILE" ]]; then
 	if [[ "$0" =~ \.sh ]]; then
-		LOGFILE=$(echo "$0" | sed 's/.*\///' | sed 's/sh$/log/')
+		shlogFile=$(echo "$0" | sed 's/.*\///' | sed 's/sh$/log/')
 	else
-		LOGFILE=$(echo "$0" | sed 's/.*\///').log
+		shlogFile=$(echo "$0" | sed 's/.*\///').log
 	fi
 else
-	unset LOGPATH
+	shlogFile="$LOGFILE"
+#	unset shlogPath
 fi
 }
 
 
-### Define the LOGPATH variable
+### Define the shlogPath variable
 shlog_vars ()
 {
-# Store variables and restore them after -p run
-LOGPATH_ORIG="$LOGPATH"
-LOGDIR_ORIG="$LOGDIR"
-LOGFILE_ORIG="$LOGFILE"
-
 # -p flag set
 if [[ -n "$shlogTmpPath" ]]; then
-	# Control variable to know if we need to restore global vars after -p run
-	shlogAltVars="1"
-
 	# nolog option
 	if [[ "$shlogTmpPath" == "nolog" ]]; then
-		LOGPATH="/dev/null"
+		shlogPath="/dev/null"
 
 	# Input is the full path
 	elif [[ "$shlogTmpPath" =~ \/([^/]+\/)+[^/]+\.[^/]{1,3} ]]; then
-		LOGPATH="$shlogTmpPath"
+		shlogPath="$shlogTmpPath"
 
 		# TODO: Validate shlogTmpPath (as full path)
 
 	# Input is a dir, append default filename
 	elif [[ "$shlogTmpPath" =~ \/([^/]+\/)+ ]]; then
-		# Validate and populate $LOGDIR
+		# Validate and populate $shlogDir
 		shlog_validate_dir "$shlogTmpPath"
 
-		# Populate $LOGFILE
+		# Populate $shlogFile
 		shlog_logfile
 
-		LOGPATH="$LOGDIR$LOGFILE"
+		shlogPath="$shlogDir$shlogFile"
 
 	# Input is a filename, append default dir
 	elif [[ "$shlogTmpPath" =~ [^/]+\.[^/]{1,3} ]]; then
-		# Populate $LOGDIR
+		# Populate $shlogDir
 		shlog_logdir
-		shlog_validate_dir "$LOGDIR"
+		shlog_validate_dir "$shlogDir"
 
-		LOGPATH="$LOGDIR$shlogTmpPath"
+		shlogPath="$shlogDir$shlogTmpPath"
 
 	fi
 
 	# TODO: Needed?
 	unset shlogTmpPath
 
-# $LOGPATH not defined
+# $shlogPath not defined
 elif [[ -z "$LOGPATH" ]]; then
-	# Get $LOGDIR and $LOGFILE
+	# Get $shlogDir and $shlogFile
 	shlog_logdir
-	shlog_validate_dir "$LOGDIR"
+	shlog_validate_dir "$shlogDir"
 
 	shlog_logfile
 
-	LOGPATH="$LOGDIR$LOGFILE"
+	shlogPath="$shlogDir$shlogFile"
 
 else
-	echo "$LOGPATH"
+	# TODO: Validate LOGPATH
+	shlogPath="$LOGPATH"
 fi
 
-# TODO: Validate $LOGPATH
+# TODO: Validate $shlogPath
 
-}
-
-
-shlog_restore_vars ()
-{
-# Store variables and restore them after -p run
-LOGPATH="$LOGPATH_ORIG"
-LOGDIR="$LOGDIR_ORIG"
-LOGFILE="$LOGFILE_ORIG"
-shlogAltVars="0"
 }
 
 
@@ -149,12 +136,16 @@ if [[ $# -eq 0 ]]; then
 	echo "Usage: shlog [-p|--path=nolog|/alternative/path/alternative.log] [-s|--stamp=timestamp|datestamp|weekstamp] \"text\"|\"\$(cmd)\""
 	exit 1
 fi
+}
 
-# TODO: Check if global vars have been set after the first run of shlog_vars
 
-### Main function
 shlog ()
 {
+# Store variables and restore them after -p run
+#LOGPATH="$shlogPath"
+#LOGDIR="$shlogDir"
+#LOGFILE="$shlogFile"
+
 # Put the arguments in variables
 while [[ $# -gt 0 ]]; do
 case "$1" in
@@ -164,10 +155,6 @@ case "$1" in
 	;;
 	-p|--path)
 		local shlogTmpPath="$2"
-
-		# Check/Set LOGPATH variable
-		shlog_vars
-
 		shift 2
 	;;
 	*)
@@ -177,30 +164,33 @@ case "$1" in
 esac
 done
 
+### Check/Set shlogPath variable
+shlog_vars
+
 # Do the thing
 if [[ -n $shlogTmpText ]]; then
 	if [[ $shlogTmpStamp == "timestamp" ]]; then
-		echo -e "$(date '+%H:%M:%S')" - "$shlogTmpText" | tee -a "$LOGPATH"
+		echo -e "$(date '+%H:%M:%S')" - "$shlogTmpText" | tee -a "$shlogPath"
 	elif [[ $shlogTmpStamp == "datestamp" ]]; then
-		echo -e "$(date '+%Y-%m-%d %H:%M:%S')" - "$shlogTmpText" | tee -a "$LOGPATH"
+		echo -e "$(date '+%Y-%m-%d %H:%M:%S')" - "$shlogTmpText" | tee -a "$shlogPath"
 	elif [[ $shlogTmpStamp == "weekstamp" ]]; then
-		echo -e "$(date '+%H:%M:%S %d-%m-%Y %a %V')" - "$shlogTmpText" | tee -a "$LOGPATH"
+		echo -e "$(date '+%H:%M:%S %d-%m-%Y %a %V')" - "$shlogTmpText" | tee -a "$shlogPath"
 	else
-		echo -e "$shlogTmpText" | tee -a "$LOGPATH"
+		echo -e "$shlogTmpText" | tee -a "$shlogPath"
 	fi
 else
 	if [[ $shlogTmpStamp == "timestamp" ]]; then
-		echo -e "$(date '+%H:%M:%S')" | tee -a "$LOGPATH"
+		echo -e "$(date '+%H:%M:%S')" | tee -a "$shlogPath"
 	elif [[ $shlogTmpStamp == "datestamp" ]]; then
-		echo -e "$(date '+%Y-%m-%d %H:%M:%S')" | tee -a "$LOGPATH"
+		echo -e "$(date '+%Y-%m-%d %H:%M:%S')" | tee -a "$shlogPath"
 	elif [[ $shlogTmpStamp == "weekstamp" ]]; then
-		echo -e "$(date '+%H:%M:%S %d-%m-%Y %a %V')" | tee -a "$LOGPATH"
+		echo -e "$(date '+%H:%M:%S %d-%m-%Y %a %V')" | tee -a "$shlogPath"
 	fi
 fi
 
-if [[ $shlogAltVars -eq 1 ]]; then
-	shlog_restore_vars
-fi
+unset shlogPath
+unset shlogDir
+unset shlogFile
 }
 
 ### Throw warning if run from a shell
